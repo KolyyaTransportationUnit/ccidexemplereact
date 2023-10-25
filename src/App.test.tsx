b46fdeb9f605d53f8ebdddb1530cff80c67e882e
Fofana Aboubacar Sidick <aboubacarsidick.fofana@kolyya.com>
2023-10-25 15:19:09 -0400

    test unit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,25 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, World!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  test('renders App component with expected content', () => {
+    render(<App />);
+
+    // Vérifie que le logo est bien affiché
+    const logoElement = screen.getByAltText('logo');
+    expect(logoElement).toBeInTheDocument();
+
+    // Vérifie que le texte d'édition est affiché
+    // const editMessage = screen.getByText(/Hello World./i);
+    // expect(editMessage).toBeInTheDocument();
+
+    // Vérifie que le lien "Learn React" est présent
+    const linkElement = screen.getByText(/Learn React/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute('href', 'https://reactjs.org');
+
+    // Vérifie que le message "Hello, World!" est affiché
+    const helloMessage = screen.getByText(/Hello, World!/i);
+    expect(helloMessage).toBeInTheDocument();
+  });
 });
