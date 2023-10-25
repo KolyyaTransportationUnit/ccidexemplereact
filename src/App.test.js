2a4fae2e0f3efb5be5f51fc695b2c56ee74f961f
Fofana Aboubacar Sidick <aboubacarsidick.fofana@kolyya.com>
2023-10-25 13:54:55 -0400

    resolve

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // pour des assertions plus sympas comme "toBeInTheDocument"
+import App from './App'; // ajustez le chemin d'importation en fonction de l'emplacement de votre fichier App.js
+
+test('vérifie que le texte "Hello, World!" est présent', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello, World!/i);
+  expect(linkElement).toBeInTheDocument();
+});
