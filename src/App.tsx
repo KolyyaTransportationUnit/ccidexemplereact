aca24471b4589950746cfe6605e34037ad8b8596
Fofana Aboubacar Sidick <aboubacarsidick.fofana@kolyya.com>
2023-10-26 09:55:51 -0400

    learn ci cd

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React i learn React typescript
         </a>
         <div className="App">
           <h1>Hello, Sidick!</h1>
