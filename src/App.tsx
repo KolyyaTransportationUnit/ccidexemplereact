878dce696494f491e49a596dccbf37dae26d2168
Fofana Aboubacar Sidick <aboubacarsidick.fofana@kolyya.com>
2023-10-26 11:13:47 -0400

    learn react //

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
           Learn React /i learn React typescript !!!
         </a>
         <div className="App">
-          <h1>Hello, Sidick! Pull request</h1>
+          <h1>Hello, Sidick fofana! Pull request</h1>
         </div>
       </header>
     </div>
